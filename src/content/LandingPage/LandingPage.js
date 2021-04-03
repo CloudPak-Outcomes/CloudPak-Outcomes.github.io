131db26a7347a5a839f53b4788298a647b16d977
ericmartens <ericmartens@gmail.com>
2021-04-02 20:35:30 -0600

    tiles on the landing page

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {ClickableTile} from "carbon-components-react";
 
 const LandingPage = () => {
     return (
         <div className="bx--grid">
             <div className="bx--row">
                 <div className="bx--col">
-                    <h1>Cloud Pak for Data Outcomes</h1>
+                    <h2>Cloud Pak for Data Outcomes</h2>
                     <p>About the Outcomes info goes here.</p>
-                    <hr />
-                    <p>Need a hero image.</p>
+                </div>
+            </div>
+            <div className="bx--row">
+                <div className="bx--col">
+                    <div className="bx--row">
+                        <div className="bx--col-lg-6">
+                            <ClickableTile href="#/employee-attrition">
+                                <h4>Employee Attrition</h4>
+                                <hr />
+                                <p>
+                                    Description here.
+                                </p>
+                            </ClickableTile>
+                        </div>
+                        <div className="bx--col-lg-6">
+                            <ClickableTile>
+                                <h4>Mortgage Default</h4>
+                                <hr />
+                                <p>
+                                    Description here.
+                                </p>
+                            </ClickableTile>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
